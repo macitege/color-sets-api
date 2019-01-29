1781c6cb0bfcfef5e37d66ec2019a99dacc6c96b
macitege <macitegeercan@gmail.com>
2019-01-29 12:10:04 -0500

    Create curl scripts for Heroku
    - Create curl scripts for creating account on Heroku
    - Create curl scripts for creating color on Heroku

---
+++ curl-scripts/auth/heroku-sign-in.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+curl "https://color-sets-api.herokuapp.com/sign-in" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --data '{
+    "credentials": {
+      "email": "'"${EMAIL}"'",
+      "password": "'"${PASSWORD}"'"
+    }
+  }'
+
+echo
